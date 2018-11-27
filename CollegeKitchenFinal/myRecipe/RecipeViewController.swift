99f8da053634bcb2905ddb71bddfd3da604da1aa
Sophie Bishopp <seb@bishopp.net>
2018-11-27 14:38:00 -0600

    Sophie Recipe small changes

---
+++ CollegeKitchenFinal/myRecipe/RecipeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeViewController.swift
+//  CollegeKitchenFinal
+//
+//  Created by Sophie Bishopp on 11/24/18.
+//  Copyright © 2018 Julia Downey. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController, UICollectionViewDataSource{
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
